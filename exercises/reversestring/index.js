b9ef383f322dfe56e31d88dd4ddfbcddf7964317
efrainbss <efrainbss@gmail.com>
2024-11-25 13:45:38 -0700

    reverse string resolved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+    let revesedString = "";
+    const separatedArr = str.split("");
+
+    for(let i = separatedArr.length - 1; i >= 0; i--)
+    {
+        revesedString += separatedArr[i];
+    }
+
+    return revesedString;
+}
 
 module.exports = reverse;
