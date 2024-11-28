797caaaa3037dce8845c909ea5b2b2b378b1352e
efrainbss <efrainbss@gmail.com>
2024-11-28 09:59:23 -0700

    maxchars updated and fizzbuzz first version completed

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,27 +5,47 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// function maxChar(str) {
+
+//     const chars = {};
+
+//     for(let char of str){
+//         chars[char] = chars[char] + 1 || 1;
+//     }
+
+//     let sortable = [];
+
+//     for (let char in chars) {
+//         sortable.push([char, chars[char]]);
+//     }
+
+//     sortable.sort(function(a, b) {
+//         return a[1] - b[1];
+//     });
+
+//     const max = sortable[sortable.length - 1][0];
+
+//     return  max;
+
+// }
+
 function maxChar(str) {
-
-    const chars = {};
-
+    const charsMap = {};
+    let max = 0;
+    let maxChar = "";
+    
     for(let char of str){
-        chars[char] = chars[char] + 1 || 1;
+        charsMap[char] = charsMap[char] + 1 || 1;
     }
 
-    let sortable = [];
-
-    for (let char in chars) {
-        sortable.push([char, chars[char]]);
+    for(let char in charsMap){
+        if(charsMap[char] > max){
+            max = charsMap[char];
+            maxChar = char
+        }
     }
 
-    sortable.sort(function(a, b) {
-        return a[1] - b[1];
-    });
-
-    const max = sortable[sortable.length - 1][0];
-
-    return  max;
+    return maxChar;
 
 }
 
