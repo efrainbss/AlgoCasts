e7d92fb2ea8a001b4e5183ebb7ccfec2991dafc2
efrainbss <efrainbss@gmail.com>
2024-12-18 11:44:51 -0700

    vowels, pyramid and matrix completed

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,89 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// function matrix(n) {
+
+//  // Create an empty NxN matrix
+//     const matrix = Array.from({ length: n }, () => Array(n).fill(0));
+ 
+//     let top = 0, bottom = n - 1;
+//     let left = 0, right = n - 1;
+//     let num = 1;
+
+//     while (top <= bottom && left <= right) {
+//         // Fill top row
+//         for (let i = left; i <= right; i++) {
+//             matrix[top][i] = num++;
+//         }
+//         top++;
+
+//         // Fill right column
+//         for (let i = top; i <= bottom; i++) {
+//             matrix[i][right] = num++;
+//         }
+//         right--;
+
+//         // Fill bottom row
+//         for (let i = right; i >= left; i--) {
+//             matrix[bottom][i] = num++;
+//         }
+//         bottom--;
+
+//         // Fill left column
+//         for (let i = bottom; i >= top; i--) {
+//             matrix[i][left] = num++;
+//         }
+//         left++;
+//     }
+
+//     return matrix;
+
+// }
+
+function matrix(n) {
+    const results = [];
+  
+    for (let i = 0; i < n; i++) {
+      results.push([]);
+    }
+  
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+    while (startColumn <= endColumn && startRow <= endRow) {
+      // Top row
+      for (let i = startColumn; i <= endColumn; i++) {
+        results[startRow][i] = counter;
+        counter++;
+      }
+      startRow++;
+  
+      // Right column
+      for (let i = startRow; i <= endRow; i++) {
+        results[i][endColumn] = counter;
+        counter++;
+      }
+      endColumn--;
+  
+      // Bottom row
+      for (let i = endColumn; i >= startColumn; i--) {
+        results[endRow][i] = counter;
+        counter++;
+      }
+      endRow--;
+  
+      // start column
+      for (let i = endRow; i >= startRow; i--) {
+        results[i][startColumn] = counter;
+        counter++;
+      }
+      startColumn++;
+    }
+  
+    return results;
+  }
+  
 
 module.exports = matrix;
--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,72 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//     const colsPerRow = n + (n - 1);
+//     let octothorpeCount = 0;
+//     let spacesCount = 0;
+  
+//     for (let row = 0; row < n; row++) {
+      
+//       let level = "";
+//       octothorpeCount = (row == 0) ? 1 : octothorpeCount + 2;
+//       spacesCount = (colsPerRow - octothorpeCount) / 2;
+      
+//       for (let col = 0; col < colsPerRow; col++) {
+        
+//         if(col <= spacesCount - 1){
+//           level += " ";
+//         }
+//         else if(col >= (octothorpeCount + spacesCount)){
+//           level += " ";
+//         }
+//         else{
+//           level += "#";
+//         }
+//       }
+  
+//       console.log(level);
+//     }
+// }
 
+// function pyramid(n) {
+    // const midpoint = Math.floor((2 * n - 1) / 2);
+
+    // for (let row = 0; row < n; row++){
+    //     let level = '';
+    //     for (let column = 0; column < 2 * n - 1; column++){
+    //         if(midpoint - row <= column && midpoint + row >= column){
+    //             level += '#';
+    //         }else{
+    //             level += ' ';
+    //         }
+    //     }
+
+    //     console.log(level);
+    // }
+
+// }
+
+function pyramid(n, row = 0, level = '') {
+    if(row === n){
+        return;
+    }
+
+    if(level.length === 2 * n - 1){
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    let add;
+
+    if(midpoint - row <= level.length && midpoint + row >= level.length){
+        add = '#';
+    }
+    else{
+        add = ' ';
+    }
+
+    pyramid(n, row, level + add);
+}
 module.exports = pyramid;
