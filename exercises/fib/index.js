a507aad074f05977198688f5cc87bb67fda40b55
efrainbss <efrainbss@gmail.com>
2025-01-06 22:15:11 -0700

    Fibobacci resolved

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,62 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// my solution
+// function fib(n) {
+//     let result = 0;
+//     let secuence = new Array(n);
+
+//     for(let i = 0; i < n; i++){
+//         if(i <= 1){
+//         	secuence[i] = i;
+//         	result = 1;
+//         }
+//         else{
+//         	secuence[i] = secuence[i - 2] + secuence[i - 1];
+//             result = secuence[i - 1] + secuence[i];
+//         }      
+//     }
+
+//     return result;  
+// }
+
+// iterative sol
+// function fib(n) {
+//     const result = [0, 1];
+
+//     for(let i = 2; i <= n; i++){
+//        const a = result[i - 1];
+//        const b = result[i - 2];
+
+//        result.push(a + b);
+//     }
+
+//     return result[n];  
+// }
+
+function memoize(fn){
+    const cache = {};
+
+    return function(...args){
+        if(cache[args]){
+            return cache[args];
+        }
+        
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+function fib(n){
+    if (n < 2){
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
